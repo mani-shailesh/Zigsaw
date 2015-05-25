b234e00fd39a4812b935daceb0c8d2d92efd3e2f
Shailesh Mani Pandey <shailesh.pandey@iitrpr.ac.in>
2015-05-25 11:05:18 +0530

    Added a timer and a screen to display the score when the game is successfully completed.

--- Assets/Scripts/CheckStatus.cs
+++ Assets/Scripts/CheckStatus.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 using UnityEngine.UI;
+using System.Diagnostics;
 public class CheckStatus : MonoBehaviour {
 	int noOfPieces = 4;
+	Stopwatch stopwatch = new Stopwatch();
+	TimeSpan timeElapsed;
 	bool isFinished = false;
+	public static Text timer;
 	Component[] slots;
 	// Use this for initialization
 	void Start () {
-
+		stopwatch.Start ();
+		timer = GameObject.Find("Timer").GetComponent<Text>();
+		timer.text = "00 : 00";
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		timeElapsed = stopwatch.Elapsed;
+		//stopwatch.Stop ();
+		timer.text = String.Format ("{0:00}:{1:00}", timeElapsed.Minutes, timeElapsed.Seconds);
+		//stopwatch.Start ();
 		slots = GetComponentsInChildren<Image> ();
 		if (slots.Length-(noOfPieces+1) == noOfPieces && isFinished==false) {
 			for (int i=3, j=1; i<=slots.Length; i+=2, j++) {
@@ -20,7 +31,9 @@ public class CheckStatus : MonoBehaviour {
 				}
 			}
 			print ("Success");
+			stopwatch.Stop();
 			isFinished=true;
+			Application.LoadLevel("result");
 		}
 	}
 }
---
+++ Assets/Scripts/displayResult.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class displayResult : MonoBehaviour {
+	Text result;
+	// Use this for initialization
+	void Start () {
+		result = GetComponent<Text> ();
+		result.text = "Yeah! You got it in " + CheckStatus.timer.text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
